4962966ed7e3aca2f8c8538df378d390ebe6556f
Rocki11 <72707602+Rocki11@users.noreply.github.com>
2021-06-02 21:10:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    
 }
 function updateRate()
 {
